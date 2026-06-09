0816d2c338d38e706ad2926a7cc30fdc19497663
Kaushik Jadav <kaushikjadav602@gmail.com>
2026-06-09 17:10:07 +0000

    Add API server skeleton

---
+++ cmd/api/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"context"
+	"log/slog"
+	"os"
+	"os/signal"
+
+	"github.com/kaushik2901/gitlab-handbook-rag-pipeline/internal/api"
+	"github.com/kaushik2901/gitlab-handbook-rag-pipeline/internal/config"
+)
+
+func main() {
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, os.Kill)
+	defer cancel()
+
+	if err := run(ctx); err != nil {
+		slog.Error("fatal", "err", err)
+		os.Exit(1)
+	}
+}
+
+func run(ctx context.Context) error {
+	cfg, err := config.Load()
+	if err != nil {
+		return err
+	}
+
+	slog.SetDefault(slog.New(slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{
+		Level: parseLogLevel(cfg.LogLevel),
+	})))
+
+	srv, err := api.New(cfg)
+	if err != nil {
+		return err
+	}
+	return srv.ListenAndServe(ctx)
+}
+
+func parseLogLevel(level string) slog.Level {
+	switch level {
+	case "debug":
+		return slog.LevelDebug
+	case "info":
+		return slog.LevelInfo
+	case "warn":
+		return slog.LevelWarn
+	case "error":
+		return slog.LevelError
+	default:
+		return slog.LevelInfo
+	}
+}
---
+++ internal/api/server.go
@@ -0,0 +1,47 @@
+package api
+
+import (
+	"context"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+
+	"github.com/kaushik2901/gitlab-handbook-rag-pipeline/internal/config"
+)
+
+const version = "0.1.0"
+
+type Server struct {
+	cfg    *config.Config
+	router *chi.Mux
+	http   *http.Server
+}
+
+func New(cfg *config.Config) (*Server, error) {
+	r := chi.NewRouter()
+	return &Server{
+		cfg:    cfg,
+		router: r,
+	}, nil
+}
+
+func (s *Server) ListenAndServe(ctx context.Context) error {
+	addr := fmt.Sprintf(":%s", config.EnvOrDefault("API_PORT", "8080"))
+	s.http = &http.Server{Addr: addr, Handler: s.router}
+
+	go func() {
+		<-ctx.Done()
+		shutdown, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+		s.http.Shutdown(shutdown)
+	}()
+
+	slog.Info("api server starting", "addr", addr)
+	if err := s.http.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		return err
+	}
+	return nil
+}
